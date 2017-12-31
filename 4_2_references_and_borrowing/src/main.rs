246304aaff259002afd428c33f11be3e4d46df79
Mario Nebl <hello@mario-nebl.de>
2017-12-31 17:30:21 +0100

    Expand with mutable references

--- 4_2_references_and_borrowing/src/main.rs
+++ 4_2_references_and_borrowing/src/main.rs
@@ -1,12 +1,23 @@
 fn main() {
-    let s1 = String::from("hello");
-    let len = calculate_length(&s1); // create a ptr "s" to ptr "s1"
+    let mut s = String::from("hello");
+    change(&mut s);
+
+    let len = calculate_length(&s); // create a ptr "s" to ptr "s1"
     // calculate_length uses a reference to s1, so
     // s1 stays valid after the scope of calculate_length was closed
 
-    println!("The length fof '{}' is {}.", s1, len);
+    println!("The length of '{}' is {}.", s, len);
 }
 
 fn calculate_length(s: &String) -> usize { // s is a reference to a String
     s.len()
+}
+
+// https://doc.rust-lang.org/error-index.html#E0596
+// fn change(s: &String) {
+//     s.push_str(", world");
+// }
+
+fn change(s: &mut String) {
+    s.push_str(", world");
 }
