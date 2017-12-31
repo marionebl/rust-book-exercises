b97e56b5a1f047be721c460f1bda9c50bb95a170
Mario Nebl <hello@mario-nebl.de>
2017-12-31 18:10:15 +0100

    Employ slices for first_word implementation

--- 4_3_slices/src/main.rs
+++ 4_3_slices/src/main.rs
@@ -1,16 +1,21 @@
 fn main() {
     let mut s = String::from("Hello world");
-    let word = first_word(&s);
+    let word = first_word_pos(&s);
+
+    {
+        let hello = first_word(&s);
+        println!("first: {}", hello);
+    }
 
     {
         let hello = &s[0..word];
-        println!("{}", hello);
+        println!("second: {}", hello);
     }
 
     s.clear();
 }
 
-fn first_word(s: &String) -> usize {
+fn first_word_pos(s: &String) -> usize {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
@@ -20,4 +25,13 @@ fn first_word(s: &String) -> usize {
     }
 
     s.len()
+}
+
+fn first_word(s: &String) -> &str {
+    for (i, &item) in s.as_bytes().iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+    &s[..]
 }
