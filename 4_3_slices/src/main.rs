9a7a60496d052504c8e4ea104fc246871c878acf
Mario Nebl <hello@mario-nebl.de>
2017-12-31 18:03:42 +0100

    Add intro to slices

---
+++ 4_3_slices/src/main.rs
@@ -0,0 +1,25 @@
+fn main() {
+    let mut s = String::from("Hello world");
+    let word = first_word(&s);
+
+    {
+        let hello = &s[0..word];
+        println!("{}", hello);
+    }
+
+    {
+        s.clear();
+    }
+}
+
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
+}
