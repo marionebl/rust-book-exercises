fcc0a8b341eddf71c772006b84fd373a381c66f8
Mario Nebl <hello@mario-nebl.de>
2018-01-01 16:12:43 +0100

    Add methods

--- 5_2_structs_example_program/src/main.rs
+++ 5_2_structs_example_program/src/main.rs
@@ -4,12 +4,21 @@ struct Rectangle {
     height: u32,
 }
 
-fn main() {
-    let rect = Rectangle { width: 30, height: 50 };
-
-    println!("The area of {rect:?} is {area} square pixels", rect=rect, area=area(&rect));
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    fn fits(&self, rect: &Rectangle) -> bool {
+        self.width > rect.width && self.height > rect.height
+    }
 }
 
-fn area(rect: &Rectangle) -> u32 {
-    rect.width * rect.height
+fn main() {
+    let rect = Rectangle { width: 30, height: 50 };
+    let rect2 = Rectangle { width: 10, height: 40 };
+    let rect3 = Rectangle { width: 60, height: 45 };
+
+    println!("The area of {rect:?} is {area} square pixels", rect=rect, area=rect.area());
+    println!("rect fits rect2 {}", rect.fits(&rect2));
+    println!("rect fits rect3 {}", rect.fits(&rect3));
 }
