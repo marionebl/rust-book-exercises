353e4576b7f297ca57a783a1d18a5efaf205bf7d
Mario Nebl <hello@mario-nebl.de>
2018-01-01 15:57:14 +0100

    Refactor to use tuples

--- 5_2_structs_example_program/src/main.rs
+++ 5_2_structs_example_program/src/main.rs
@@ -1,10 +1,9 @@
 fn main() {
-    let width = 30;
-    let height = 50;
+    let rect = (30, 50);
 
-    println!("The area is {} square pixels", area(width, height));
+    println!("The area is {} square pixels", area(rect));
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+fn area(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
 }
