1230bc8ca8eb5d94c1a451201773f6a444de321a
Mario Nebl <hello@mario-nebl.de>
2018-01-01 16:37:33 +0100

    Implement display trait for IpAddr

--- 6_1_enum_intro/src/main.rs
+++ 6_1_enum_intro/src/main.rs
@@ -1,13 +1,24 @@
+use std::fmt;
+
 #[derive(Debug)]
 enum IpAddr {
-    V4(String),
+    V4(u8, u8, u8, u8),
     V6(String),
 }
 
+impl fmt::Display for IpAddr {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            &IpAddr::V4(a, b, c, d) => write!(f, "{}.{}.{}.{}", a, b, c, d),
+            &IpAddr::V6(ref a) => write!(f, "{}", a)
+        }
+    }
+}
+
 fn main() {
-    let home = IpAddr::V4(String::from("127.0.0.1"));
+    let home = IpAddr::V4(127, 0, 0, 1);
     let loopback = IpAddr::V6(String::from("::1"));
 
-    println!("v4: {ip:?}", ip=home);
-    println!("v6: {ip:?}", ip=loopback);
+    println!("v4: {ip}", ip=home);
+    println!("v6: {ip}", ip=loopback);
 }
