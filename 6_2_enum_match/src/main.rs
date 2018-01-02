ea9b2a4f2b9826ea9f457152b3add8fd9f1eaca3
Mario Nebl <hello@mario-nebl.de>
2018-01-02 20:03:10 +0100

    Add Option example

--- 6_2_enum_match/src/main.rs
+++ 6_2_enum_match/src/main.rs
@@ -41,4 +41,17 @@ fn main() {
     println!("{}", nickel.value());
     println!("{}", alabama_quarter.value());
     println!("{}", alaska_quarter.value());
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    println!("six: {:?}, none: {:?}", six, none);
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1)
+    }
 }
