54ede9c5af4eb355afb6000a8a111c616fa082eb
Mario Nebl <hello@mario-nebl.de>
2018-01-07 11:58:06 +0100

    Demo direct reference

--- 7_3_importing_names/src/main.rs
+++ 7_3_importing_names/src/main.rs
@@ -2,14 +2,16 @@ pub mod a {
     pub mod series {
         pub mod of {
             pub fn nested_modules() {
-
+                println!("Nested all the way");
             }
         }
     }
 }
 
 use a::series::of;
+use a::series::of::nested_modules;
 
 fn main() {
     of::nested_modules();
+    nested_modules();
 }
