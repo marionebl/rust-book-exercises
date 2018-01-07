707079a707a4d5a06087c91f3ea3b5ac1723506e
Mario Nebl <hello@mario-nebl.de>
2018-01-07 12:04:18 +0100

    Explore use * and pitfalls

--- 7_3_importing_names/src/main.rs
+++ 7_3_importing_names/src/main.rs
@@ -14,15 +14,24 @@ enum TrafficLight {
     Green,
 }
 
+enum MinimalTrafficLight {
+    Red,
+    Green
+}
+
 use a::series::of;
 use a::series::of::nested_modules;
 use TrafficLight::{Red, Yellow};
+use MinimalTrafficLight::*; // This shadows 'Red' from TrafficLight!
 
 fn main() {
     of::nested_modules();
     nested_modules();
 
-    let _red = Red;
+    let _red = Red; // References MinimalTrafficLight::Red
     let _yellow = Yellow;
     let _green = TrafficLight::Green;
+
+    let _minimal_green = Green;
+    let _minimal_red = Red;
 }
