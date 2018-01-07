e2f767e45df0c57cbf4fb349e8d9cd29707016bd
Mario Nebl <hello@mario-nebl.de>
2018-01-07 12:29:01 +0100

    Show invalid references and possible solution

--- 8_1_vectors/src/main.rs
+++ 8_1_vectors/src/main.rs
@@ -28,4 +28,26 @@ fn main() {
     let non_existant_get = v.get(1000);
 
     println!("non_existant_get: {:?}", non_existant_get);
+
+    invalid_references();
+}
+
+fn invalid_references() {
+    let mut v = Vec::new();
+
+    v.push(5);
+    v.push(6);
+    v.push(7);
+    v.push(8);
+
+    // https://doc.rust-lang.org/error-index.html#E0502
+    // let first = &v[0];
+
+    {
+        // solution: descope before mutation
+        let scoped =  &v[0];
+        println!("invalid_references scoped {}", scoped);
+    }
+
+    v.push(9);
 }
