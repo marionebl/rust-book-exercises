5195d82acb813e0b80b55d101c931cdc21207bc6
Mario Nebl <hello@mario-nebl.de>
2018-01-09 18:55:31 +0100

    Review ways to init a String

---
+++ 8_2_strings/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let mut s = String::new();
+
+    let data = "initial contents";
+    let t = data.to_string();
+    let u = "initial_contents".to_string();
+
+    let v = String::from(data);
+}
