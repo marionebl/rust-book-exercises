2d01e302c518fe461bcdce30431606d5ba9c721e
Mario Nebl <hello@mario-nebl.de>
2018-01-09 18:58:31 +0100

    Show literals being &str

--- 8_2_strings/src/main.rs
+++ 8_2_strings/src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
     let mut _s = String::new();
-    _s.push_str("bar");
+    let _bar = "bar";
+    _s.push_str(&_bar);
+    println!("_s is {}, _bar is {}", _s, _bar);
 
     let data = "initial contents";
     let _t = data.to_string();
