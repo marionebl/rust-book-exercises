028024a86bd0b345c10aa30cec1e7bf94a2aecf8
Mario Nebl <hello@mario-nebl.de>
2018-01-10 12:39:58 +0100

    Begin exploration of ownership in hashmaps

--- 8_3_hashmaps/src/main.rs
+++ 8_3_hashmaps/src/main.rs
@@ -14,4 +14,15 @@ fn main() {
     let scores_two: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
 
     println!("scores_two: {:?}", scores_two);
+
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+
+    println!("map: {:?}", map);
+
+    // https://doc.rust-lang.org/error-index.html#E0382
+    // println!("field_name: {}, field_value: {}", field_name, field_value);
 }
