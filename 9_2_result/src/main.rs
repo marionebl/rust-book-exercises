0c3019e31a39ad3d1edccaaee1e503e9542c5ff5
Mario Nebl <hello@mario-nebl.de>
2018-01-14 12:28:12 +0100

    Factor out open_or_create

--- 9_2_result/src/main.rs
+++ 9_2_result/src/main.rs
@@ -1,21 +1,21 @@
 use std::fs;
 use std::fs::File;
+use std::io;
 use std::io::ErrorKind;
 
 fn main() {
     let name = "hello.txt";
 
-    let _f = match File::open(name) {
-        Ok(file) => file,
-        // Thing(ref value) Match value, provide reference
-        // Thing(&value) Match reference
-        Err(ref error) if error.kind() == ErrorKind::NotFound => { // Match guard
-            File::create(name).expect("Failed to create hello.txt")
-        },
-        Err(error) => {
-            panic!("Problem while opening {}: {:?}", name, error)
-        }
-    };
-
+    let _f = open_or_create(name);
     fs::remove_file(name).expect("Failed cleaning up hello.txt");
+}
+
+fn open_or_create(filename: &str) -> Result<File, io::Error> {
+    match File::open(filename) {
+        Ok(file) => Ok(file),
+        Err(ref error) if error.kind() == ErrorKind::NotFound => {
+            File::create(filename)
+        },
+        Err(error) => Err(error)
+    }
 }
