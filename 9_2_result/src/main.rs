7ae1549826724b9433c739f54880519d0fffe30a
Mario Nebl <hello@mario-nebl.de>
2018-01-14 12:16:25 +0100

    Expand error handling

--- 9_2_result/src/main.rs
+++ 9_2_result/src/main.rs
@@ -1,13 +1,29 @@
+use std::fs;
 use std::fs::File;
+use std::io::ErrorKind;
 
 fn main() {
-    let f = File::open("hello.txt");
+    let name = "hello.txt";
 
-    // Equivalent to let _f = f.unwrap();
-    let _f = match f {
+    let _f = match File::open(name) {
         Ok(file) => file,
+        Err(ref error) if error.kind() == ErrorKind::NotFound => { // oO
+            match File::create(name) {
+                Ok(file) => file,
+                Err(e) => {
+                    panic!("Tried to create {} but there was a problem: {:?}", name, e)
+                }
+            }
+        },
         Err(error) => {
-            panic!("There was a problem opening the file: {:?}", error)
+            panic!("Problem while opening {}: {:?}", name, error)
         }
     };
+
+    match fs::remove_file(name) {
+        Ok(_) => (),
+        Err(error) => {
+            panic!("Problem while cleaning up {}: {:?}", name, error)
+        }
+    }
 }
