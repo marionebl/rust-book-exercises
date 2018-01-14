5a8f760f30157756d8fd8c616e98bf5722fa04fa
Mario Nebl <hello@mario-nebl.de>
2018-01-14 12:21:05 +0100

    Refactor to use unwrap() where applicable

--- 9_2_result/src/main.rs
+++ 9_2_result/src/main.rs
@@ -10,22 +10,12 @@ fn main() {
         // Thing(ref value) Match value, provide reference
         // Thing(&value) Match reference
         Err(ref error) if error.kind() == ErrorKind::NotFound => { // Match guard
-            match File::create(name) {
-                Ok(file) => file,
-                Err(e) => {
-                    panic!("Tried to create {} but there was a problem: {:?}", name, e)
-                }
-            }
+            File::create(name).unwrap()
         },
         Err(error) => {
             panic!("Problem while opening {}: {:?}", name, error)
         }
     };
 
-    match fs::remove_file(name) {
-        Ok(_) => (),
-        Err(error) => {
-            panic!("Problem while cleaning up {}: {:?}", name, error)
-        }
-    }
+    fs::remove_file(name).unwrap();
 }
