f41002ca454552f6e16eaeb75893505e1bcade2a
Mario Nebl <hello@mario-nebl.de>
2018-01-16 00:21:21 +0100

    Show usage of generics with trait bounds

--- 10_1_generics/src/main.rs
+++ 10_1_generics/src/main.rs
@@ -1,29 +1,20 @@
+use std::cmp::PartialOrd;
+use std::clone::Clone;
+
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
-    let result = largest_i32(&number_list);
+    let result = largest(&number_list);
     println!("The largest number is {}", result);
 
     let char_list = vec!['y', 'm', 'c', 'a'];
-    let result = largest_char(&char_list);
+    let result = largest(&char_list);
     println!("The largest char is {}", result);
 }
 
-fn largest_i32(list: &[i32]) -> i32 {
-    let mut largest = list[0];
+fn largest<T: PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
 
-    for &item in list.iter() {
-        if item > largest {
-            largest = item;
-        }
-    }
-
-    largest
-}
-
-fn largest_char(list: &[char]) -> char {
-    let mut largest = list[0];
-
-    for &item in list.iter() {
+    for item in list.iter() {
         if item > largest {
             largest = item;
         }
