dd61bd8b967393b4395d05f80e9ffbecac148289
Mario Nebl <hello@mario-nebl.de>
2018-01-16 18:34:36 +0100

    Show methods with generic types

---
+++ 10_1_generics_methods/src/main.rs
@@ -0,0 +1,15 @@
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+fn main() {
+    let p = Point { x: 5, y: 10 };
+    println!("p.x = {}", p.x());
+}
