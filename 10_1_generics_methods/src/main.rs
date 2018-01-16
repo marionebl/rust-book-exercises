9db4e9081ca9e09590a7bc87f7a1f84dacd2af05
Mario Nebl <hello@mario-nebl.de>
2018-01-16 18:38:27 +0100

    Demonstrate concrete type implementations

--- 10_1_generics_methods/src/main.rs
+++ 10_1_generics_methods/src/main.rs
@@ -9,7 +9,21 @@ impl<T> Point<T> {
     }
 }
 
+impl Point<f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
 fn main() {
     let p = Point { x: 5, y: 10 };
     println!("p.x = {}", p.x());
+
+    // E0599 
+    // p.distance_from_origin();
+
+    let p = Point { x: 5.0, y: 10.0 };
+    let d = p.distance_from_origin();
+
+    println!("d = {}", d);
 }
